95ef6ba45286a74eb990069dd4667f5542401057
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-22 12:25:20 -0600

    move onSubmit from wrapper to form element

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -12,9 +12,9 @@ export default function AuthForm({ text }) {
   }
 
   return (
-    <div className='auth' onSubmit={handleSubmit}>
+    <div className='auth'>
       <h3 className='auth__title'>{text.title}</h3>
-      <form className='auth__form' method='POST' name={text.formName}>
+      <form className='auth__form' onSubmit={handleSubmit} method='POST' name={text.formName}>
         <input className='auth__input' onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' name='email' required/>
         <input className='auth__input' onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' name='password' required />
         <button className='auth__submit' type='submit'>{text.title}</button>
