911a42bdf095f879c383d735e025fc0b74a1fda5
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-24 11:08:43 -0600

    refactor header classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,15 @@ export default function Header({ loggedIn, userEmail, logout }) {
     <header className='header'>
       <div className='logo'/>
       {
-        (location.pathname === '/login') && <a className='header__link' href='/register'>Sign up</a>
+        (location.pathname === '/login') && <a className='header__text header__text_link' href='/register'>Sign up</a>
       }
       {
-        (location.pathname === '/register') && <a className='header__link' href='/login'>Sign in</a>}
+        (location.pathname === '/register') && <a className='header__text header__text_link' href='/login'>Sign in</a>}
       {
         (loggedIn && location.pathname === '/') && 
         <div className='header__logged-in'>
-          <p className='header__email'>{userEmail}</p>
-          <a className='header__link header__link_log-out' onClick={logout} href='/login'>Log out</a>
+          <p className='header__text header__text_email'>{userEmail}</p>
+          <a className='header__text header__text_link header__text_log-out' onClick={logout} href='/login'>Log out</a>
         </div>
       }
     </header>
