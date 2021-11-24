3b506323cb7eeae9d7b08c59680de3a9b927fd9e
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-24 00:20:45 -0600

    fix link routes for AuthForm

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
 export default function AuthForm({ text, onSubmit }) {
+  let location = useLocation();
 
   const [email,setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -18,7 +20,7 @@ export default function AuthForm({ text, onSubmit }) {
         <input className='auth__input' onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' name='email' required/>
         <input className='auth__input' onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' name='password' required />
         <button className='auth__submit' type='submit'>{text.title}</button>
-        <p className='auth__redirect'>{text.redirect} <a className='auth__link' href='/'>here!</a>
+        <p className='auth__redirect'>{text.redirect} <a className='auth__link' href={location.pathname === '/login' ? '/register' : '/login'}>here!</a>
         </p>
       </form>
     </div>
