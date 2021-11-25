0a942ba10042ecc2f3f7b6c635af99abde406ed2
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-25 17:50:12 -0600

    move AuthForm to Login comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import AuthForm from './AuthForm';
 
 export default function Login({ onSignIn }) {
 
-  const logInText = {
-    title: 'Log in',
-    formName: 'login',
-    redirect: 'Not a member yet? Sign up'
+  const [email,setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSignIn(email, password);
   }
 
   return (
-    <AuthForm text={logInText} onSubmit={onSignIn} />
+    <div className='auth'>
+      <h3 className='auth__title'>Log in</h3>
+      <form className='auth__form' onSubmit={handleSubmit} method='POST' name='login'>
+        <input className='auth__input' onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' name='email' required/>
+        <input className='auth__input' onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' name='password' required />
+        <button className='auth__submit' type='submit'>Log in</button>
+        <p className='auth__redirect'>Not a member yet? Sign up <a className='auth__link' href='/register'>here!</a>
+        </p>
+      </form>
+    </div>
   )
 }
