d80db192612b4a0ec70358eb79a96af41c8062ce
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-25 17:46:23 -0600

    transfer AuthForm stuff to Register comp

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import AuthForm from './AuthForm';
 
 export default function Register({ onRegister }) {
 
-  const registerText = {
-    title: 'Sign up',
-    formName: 'register',
-    redirect: 'Already a member? Log in'
+  const [email,setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onRegister(email, password);
   }
 
   return (
-    <AuthForm text={registerText} onSubmit={onRegister} />
+    <div className='auth'>
+      <h3 className='auth__title'>Sign up</h3>
+      <form className='auth__form' onSubmit={handleSubmit} method='POST' name='register'>
+        <input className='auth__input' onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' name='email' required/>
+        <input className='auth__input' onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' name='password' required />
+        <button className='auth__submit' type='submit'>Sign up</button>
+        <p className='auth__redirect'>Already a member? Log in <a className='auth__link' href='/login'>here!</a>
+        </p>
+      </form>
+    </div>
   )
 }
