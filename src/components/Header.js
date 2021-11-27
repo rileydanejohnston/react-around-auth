3090d24a434c8f823785ed284dff4ba445398d45
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-26 20:59:29 -0600

    add Link to header, remove duplicate tags, change variable to const

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useLocation } from 'react-router'
+import { Link } from 'react-router-dom';
 
 export default function Header({ loggedIn, userEmail, logout }) {
-  let location = useLocation();
+  const location = useLocation();
 
   const [menuOpen, setMenuOpen] = React.useState(false);
 
@@ -10,20 +11,20 @@ export default function Header({ loggedIn, userEmail, logout }) {
 
   return (
     <header className={loggedIn ? 'header header_logged-in' : 'header'}>
-      <header className='header__menu-wrapper'>
+      <div className='header__menu-wrapper'>
         <div className='logo'/>
         {loggedIn && <button className={menuOpen ? `header__button header__button_close` : `header__button header__button_menu`} onClick={() => setMenuOpen(!menuOpen)}/>}
-      </header>
+      </div>
       {
-        (location.pathname === '/login') && <a className='header__text header__text_link header__text_auth' href='/register'>Sign up</a>
+        (location.pathname === '/login') && <Link className='header__text header__text_link header__text_auth' to='/register'>Sign up</Link>
       }
       {
-        (location.pathname === '/register') && <a className='header__text header__text_link header__text_auth' href='/login'>Log in</a>}
+        (location.pathname === '/register') && <Link className='header__text header__text_link header__text_auth' to='/login'>Log in</Link>}
       {
         (loggedIn && location.pathname === '/') && 
         <div className={menuOpen ? 'header__nav header__nav_active' : 'header__nav'}>
           <p className='header__text header__text_email'>{userEmail}</p>
-          <a className='header__text header__text_link header__text_log-out' onClick={logout} href='/login'>Log out</a>
+          <Link className='header__text header__text_link header__text_log-out' onClick={logout} to='/login'>Log out</Link>
         </div>
       }
     </header>
