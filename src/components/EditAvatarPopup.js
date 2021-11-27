b67443ab2914dbb1f11a1ee885103ab157122cc1
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-26 20:46:40 -0600

    destructure props for EditAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PopupWithForm from './PopupWithForm'
 
-function EditAvatarPopup(props) {
+function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
 
   const avatarRef = React.useRef('');
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onUpdateAvatar(avatarRef.current.value);
+    onUpdateAvatar(avatarRef.current.value);
   }
 
   return (
-    <PopupWithForm title='Change profile picture' name='avatar' isOpen={props.isOpen} onSubmit={handleSubmit} onClose={props.onClose}>
+    <PopupWithForm title='Change profile picture' name='avatar' isOpen={isOpen} onSubmit={handleSubmit} onClose={onClose}>
       <input className='popup__about popup__about_profile-pic popup__input' ref={avatarRef}  id='profile-pic-url' type='url' placeholder='Image link' name='pic' required />
       <span className='popup__error' id='profile-pic-url-error' />
     </PopupWithForm>
